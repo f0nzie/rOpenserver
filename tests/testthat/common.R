620c8deba67b8b0082415bedc327d681822d420b
fonzie zbook <alfonso.reyes@oilgainsanalytics.com>
2020-06-29 11:45:57 -0700

    new function get_all_models_by_ext() returns a dataframe of base_name and full_name. additional object models to grab all models instead of one.

--- tests/testthat/common.R
+++ tests/testthat/common.R
@@ -1,4 +1,4 @@
-
+library(dplyr)
 
 oserver_methods <- c("server", ".__enclos_env__", "clone", "app_name",
                      "DoSet", "DoGet", "DoCmd", "initialize", "DoSlowCmd",
@@ -6,9 +6,23 @@ oserver_methods <- c("server", ".__enclos_env__", "clone", "app_name",
 
 get_model_from_pkg <- function(mod_num = 1, ext = c("out", "gap", "mbi")) {
     app_dir <- system.file("models", package = "rOpenserver")
-    model <- list.files(app_dir, pattern = paste0("*.", ext), ignore.case = TRUE)[mod_num]   # the first
+    models <- list.files(app_dir, pattern = paste0("*.", ext), ignore.case = TRUE)
+    model <- models[mod_num]  # the first
     file <- file.path(app_dir, model)
     if (!file.exists(file)) stop("Model not found ...") else
         return(file)
+
+}
+
+get_all_models_by_ext <- function(ext = c("out", "gap", "mbi")) {
+    app_dir <- system.file("models", package = "rOpenserver")
+    full_name <- list.files(app_dir, pattern = paste0("*.", ext),
+                              ignore.case = TRUE, full.names = TRUE)
+    base_name <- basename(full_name)
+    # very important to sort but first convert to lowercase
+    arrange(data.frame(base_name = base_name, full_name = full_name,
+                       stringsAsFactors = FALSE),
+            tolower(base_name))
+
 
 }
