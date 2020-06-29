efb96162eb16d53c5ecbbe4b89672bf4e46dc3d1
fonzie zbook <alfonso.reyes@oilgainsanalytics.com>
2020-06-28 18:12:17 -0700

    replace delay using Sys.sleep() in tests with while loop because sleep() doesn't work in this case. rename.

--- tests/testthat/test-status.R
+++ tests/testthat/test-status.R
@@ -1,5 +1,6 @@
 library(testthat)
 library(rOpenserver)
+library(tictoc)
 
 context("OpenServer Windows processes")
 
@@ -11,25 +12,29 @@ test_that("Windows processes are captured", {
     cmd = "PROSPER.START"
     prosper_server$DoCmd(cmd)
 
-    status <- read_openserver_status()
+    verbose <- FALSE
+    status <- read_openserver_status(verbose = verbose)  # status dataframe
+
 
     # print(unique(status$process))
     expect_true(any(grepl("prosper", unique(status$process))))
     expect_true(any(grepl("px32COM10", unique(status$process))))
 
-    Sys.sleep(4)
+    #Sys.sleep(2)
     cmd = "PROSPER.SHUTDOWN"
     prosper_server$DoCmd(cmd)
 
     # tests that after shutdown some processes remain
     status <- read_openserver_status()
+    if (verbose) print(status)
     expect_true(any(grepl("pxserver.exe", unique(status$process))))
     expect_true(any(grepl("PxLs.exe", unique(status$process))))
-    expect_true(any(grepl("px32COM10", unique(status$process))))
+    expect_true(any(grepl("px32COM10.exe", unique(status$process))))
 
     # couple of processes remain after NULL
     prosper_server <- NULL
     status <- read_openserver_status()
+    if (verbose) print(status)
     expect_true(any(grepl("pxserver.exe", unique(status$process))))
     expect_true(any(grepl("PxLs.exe", unique(status$process))))
 
@@ -38,27 +43,28 @@ test_that("Windows processes are captured", {
     # prosper_server <- NULL
     rm(prosper_server)
     status <- read_openserver_status()
+    if (verbose) print(status)
+    expect_true(any(grepl("pxserver.exe", unique(status$process))))
+    expect_true(any(grepl("PxLs.exe", unique(status$process))))
     expect_false(any(grepl("prosper", unique(status$process))))
     expect_false(any(grepl("px32COM10", unique(status$process))))
 
-
+    # wait for PxLs.exe process to shut
     wait_for_pxserver <- function(t = 90, ...) {
-        sec = 1
         x <- sort(runif(t))
         pb <- txtProgressBar(...)
         for(i in c(0, x, 1)) {
-            # cat(i)
-            Sys.sleep(sec)
+            # 1 second delay: Sys.sleep() will not work here
+            date_time <- Sys.time()
+            while((as.numeric(Sys.time()) - as.numeric(date_time)) < 1.0){}
             setTxtProgressBar(pb, i)
         }
-        Sys.sleep(2)
         close(pb)
         status <- read_openserver_status()
         expect_false(any(grepl("PxLs.exe", unique(status$process))))
         expect_false(any(grepl("pxserver", unique(status$process))))
     }
 
-    # wait_for_pxserver(t = 360, style = 3)
-
+    wait_for_pxserver(t = 23, style = 3)
 })
 
