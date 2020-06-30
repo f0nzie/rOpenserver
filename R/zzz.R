cc237a138c5387f50f4d788820d6aa9901714a7d
fonzie zbook <alfonso.reyes@oilgainsanalytics.com>
2020-06-30 16:21:36 -0700

    blank lines

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,10 @@
+
+
+
 # Enable environment for allocation of S3 methods from R6
 # these S3 methods will not be able to be tested during unit tests
 # because of the environment where they are stored
-R62S3::R62Fun(OpenServer, assignEnvir = topenv(), mask = T)
+R62S3::R62Fun(.OpenServer, assignEnvir = topenv(), mask = T)
 
 
 # .onAttach <- function(libname, pkgname) {
@@ -12,3 +15,4 @@ R62S3::R62Fun(OpenServer, assignEnvir = topenv(), mask = T)
 #         "\n\nGet started:\t?rOpenserver")
 #     packageStartupMessage("-----------------------------\n")
 # }
+
