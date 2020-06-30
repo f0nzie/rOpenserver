f81d083c232fe57389af169dcacfd6e666cf57f5
fonzie zbook <alfonso.reyes@oilgainsanalytics.com>
2020-06-30 16:21:14 -0700

    move createCOMReference from core.R

--- R/openserver-package.R
+++ R/openserver-package.R
@@ -1,2 +1,23 @@
 #' @keywords internal
 "_PACKAGE"
+
+
+
+
+
+
+
+#' @title COM Reference call
+#'
+#' @description Call COM client reference function
+#'
+#' @param ref the reference to pass
+#' @param className the name of the class
+#' @export
+#' @keywords internal
+#' @import RDCOMClient
+createCOMReference <- function(ref, className) {
+    RDCOMClient::createCOMReference(ref, className)
+}
+
+
