648255bf1a8eed8f83522d9a3473ffbf06d6cfee
fonzie zbook <alfonso.reyes@oilgainsanalytics.com>
2020-06-30 16:23:33 -0700

    add function sleep_well() instead of using Sys.sleep which sometimes doesn't work

--- tests/testthat/test-status.R
+++ tests/testthat/test-status.R
@@ -1,6 +1,12 @@
 library(testthat)
 library(rOpenserver)
-library(tictoc)
+
+
+sleep_well <- function(seconds = 1.0) {
+    date_time <- Sys.time()
+    seconds <- as.numeric(seconds)
+    while((as.numeric(Sys.time()) - as.numeric(date_time)) < seconds) {}
+}
 
 context("OpenServer Windows processes")
 
@@ -15,7 +21,6 @@ test_that("Windows processes are captured", {
     verbose <- FALSE
     status <- read_openserver_status(verbose = verbose)  # status dataframe
 
-
     # print(unique(status$process))
     expect_true(any(grepl("prosper", unique(status$process))))
     expect_true(any(grepl("px32COM10", unique(status$process))))
@@ -27,6 +32,7 @@ test_that("Windows processes are captured", {
     # tests that after shutdown some processes remain
     status <- read_openserver_status()
     if (verbose) print(status)
+    sleep_well(3)
     expect_true(any(grepl("pxserver.exe", unique(status$process))))
     expect_true(any(grepl("PxLs.exe", unique(status$process))))
     expect_true(any(grepl("px32COM10.exe", unique(status$process))))
@@ -56,7 +62,7 @@ test_that("Windows processes are captured", {
         for(i in c(0, x, 1)) {
             # 1 second delay: Sys.sleep() will not work here
             date_time <- Sys.time()
-            while((as.numeric(Sys.time()) - as.numeric(date_time)) < 1.0){}
+            while((as.numeric(Sys.time()) - as.numeric(date_time)) < 1.0) {}
             setTxtProgressBar(pb, i)
         }
         close(pb)
@@ -65,6 +71,6 @@ test_that("Windows processes are captured", {
         expect_false(any(grepl("pxserver", unique(status$process))))
     }
 
-    wait_for_pxserver(t = 23, style = 3)
+    wait_for_pxserver(t = 27, style = 3)
 })
 
